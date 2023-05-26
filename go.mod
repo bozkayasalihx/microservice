628ee553d27480e6c9d25ae194efd90a4fdf8f41
bozkayasalihx <jamesgosling10@gmail.com>
2023-05-27 02:31:21 +0300

    micservice init

---
+++ go.mod
@@ -0,0 +1,59 @@
+module microservice
+
+go 1.20
+
+require (
+	github.com/gin-contrib/cors v1.4.0
+	github.com/gin-gonic/gin v1.9.0
+	github.com/jackc/pgx/v4 v4.18.1
+	github.com/joho/godotenv v1.5.1
+	github.com/uid4oe/microservices-go-grpc v0.0.0-20220813091440-eee7c910d979
+	go.mongodb.org/mongo-driver v1.11.6
+	google.golang.org/grpc v1.55.0
+	google.golang.org/protobuf v1.30.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
+
+require (
+	github.com/bytedance/sonic v1.8.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ user/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"log"
+	"microservice/user/userdb"
+	"net"
+	"time"
+
+	"github.com/uid4oe/microservices-go-grpc/user/userpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	timeout = time.Second
+)
+
+type server struct {
+	userpb.UnimplementedUserServiceServer
+}
+
+func (*server) CreateUpdateUser(ctx context.Context, req *userpb.CreateUpdateUserRequest) (*userpb.CreateUpdateUserResponse, error) {
+	log.Println("Called CreateUpdateUser Operation", req.Operation)
+
+	c, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	var uid primitive.ObjectID
+	if req.Operation == userpb.Operation_CREATE {
+		uid = primitive.NewObjectID()
+	} else {
+		var err error
+		uid, err = primitive.ObjectIDFromHex(req.Id)
+		if err != nil {
+			return nil, error_response(err)
+		}
+	}
+
+	err := userdb.UpsertOne(c, &userdb.User{
+		Id: uid, Name: req.Name,
+		Age: req.Age, Greeting: req.Greeting,
+		Salary: req.Salary, Power: req.Power})
+	if err != nil {
+		return nil, error_response(err)
+	}
+
+	return &userpb.CreateUpdateUserResponse{Id: uid.Hex()}, nil
+}
+
+func (*server) GetUserDetails(ctx context.Context, req *userpb.GetUserDetailsRequest) (*userpb.GetUserDetailsResponse, error) {
+	log.Println("Called GetUserDetails, Id", req.Id)
+
+	c, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	uid, err := primitive.ObjectIDFromHex(req.GetId())
+	if err != nil {
+		return nil, error_response(err)
+	}
+
+	result, err := userdb.FindOne(c, uid)
+	if err != nil {
+		return nil, error_response(err)
+	}
+
+	return &userpb.GetUserDetailsResponse{Salary: result.Salary, Power: result.Power}, nil
+}
+
+func (*server) GetUsers(ctx context.Context, req *userpb.GetUsersRequest) (*userpb.GetUsersResponse, error) {
+	log.Println("Called GetUsers")
+
+	c, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	data, err := userdb.Find(c)
+
+	if err != nil {
+		return nil, error_response(err)
+	}
+
+	var res userpb.GetUsersResponse
+	for _, d := range *data {
+		res.Users = append(res.Users, &userpb.GetUserResponse{Id: d.Id.Hex(), Name: d.Name, Age: d.Age, Greeting: d.Greeting})
+	}
+
+	return &res, nil
+}
+
+func error_response(err error) error {
+	log.Println("ERROR:", err.Error())
+	return status.Error(codes.Internal, err.Error())
+}
+
+func main() {
+	log.Println("User Service")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
+
+	userdb.Mongo_Client, err = userdb.NewClient(context.Background())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer userdb.Mongo_Client.Disconnect(context.Background())
+
+	s := grpc.NewServer()
+	userpb.RegisterUserServiceServer(s, &server{})
+
+	log.Printf("Server started at %v", lis.Addr().String())
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Println("ERROR:", err.Error())
+	}
+}
